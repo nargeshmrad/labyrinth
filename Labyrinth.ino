286c9d264f4a50b124e65695633f92e3a6ad1661
Pierre Rossel <>
2023-01-13 13:41:58 +0100

    Split Pierre and Narges configuration

--- Labyrinth.ino
+++ Labyrinth.ino
@@ -1,3 +1,7 @@
+// Uncomment only one of these includes
+#include "StatesNarges.h"
+//#include "StatesPierre.h"
+
 const int LIGHT_SENSOR_PIN = A0;  // Arduino pin connected to light sensor's  pin
 const int LIGHT_THRESHOLD = 40;
 const int LIGHT_DEBOUNCE = 5;
@@ -7,45 +11,6 @@ bool touch = false;
 int state = 0;
 int blinkingState = 0;
 unsigned long stateStartMs = 0;
-
-// Define which led is lit (1) for each state
-word ledStates[] = {
-  //1111111000000000  
-  //6543210987654321  <- Led number
-  0b0000000000000000, // state 0
-  0b0000000000000101, // 1
-  0b0000000000000110, // 2
-  0b0000000000000100, // 3
-  0b0000000000001100, // 4
-  0b0000000000110000, // 5
-  0b0000000000100000, // 6
-  0b0000000001100000, // 7
-  0b0000000010100000, // 8
-  //1111111000000000
-  //6543210987654321  <- Led number
-};
-
-// Define the transitions between states
-// Each line represent a state and contains the possible next state transition
-// Must be 2 value. If only one choice is available, put it twice.
-byte transitions[][2] = {
-  {3, 6}, // state 0 Can go to state 3 or 6
-  {0, 0}, // state 1 End of game, back to state 0
-  {1, 1}, // state 2 Can go to state 1 only (twice the same)
-  {2, 4}, // state 3
-  {0, 0}, // state 4 
-  {0, 0}, // state 5
-  {5, 7}, // state 6
-  {8, 8}, // state 7
-  {0, 0}, // state 8
-};
-
-byte ledPins[] = {
-  2, 3, 4, 5, 6, 7, 8, 9
-};
-int nLeds = sizeof(ledPins);
-
-//int LEDIndex = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -71,17 +36,12 @@ void loop() {
       changeState(blinkingState);
     }
 
-    //digitalWrite(ledPins[LEDIndex], HIGH); // turn on LED
-    //toggleNextLED();
-
   } else if (light > LIGHT_THRESHOLD + LIGHT_DEBOUNCE && touch) {
     touch = false;
     Serial.println("UP");
-    //digitalWrite(ledPins[LEDIndex], LOW);  // turn off LED
   }
 
   // Update leds
-  //Serial.println(String("state: ") + state);
   updateBlinkState();
 
   word stateLeds = ledStates[state];
@@ -118,15 +78,6 @@ void initAllLEDs() {
 //      digitalWrite(ledPins[i], LOW);
 //    }
 //  }
-//}
-
-//void toggleNextLED() {
-//  LEDIndex++;
-//  if (LEDIndex >= nLeds) {
-//    LEDIndex = 0;
-//  }
-//  Serial.println(String("led ") + LEDIndex + " pin " + ledPins[LEDIndex]);
-//  powerOnSelectedLEDOnly(LEDIndex);
 //}
 
 void changeState(int newState) {
---
+++ StatesNarges.h
@@ -0,0 +1,37 @@
+
+// Define which led is lit (1) for each state
+word ledStates[] = {
+  //1111111000000000  
+  //6543210987654321  <- Led number
+  0b0000000000000000, // state 0
+  0b0000000000000101, // 1
+  0b0000000000000110, // 2
+  0b0000000000000100, // 3
+  0b0000000000001100, // 4
+  0b0000000000110000, // 5
+  0b0000000000100000, // 6
+  0b0000000001100000, // 7
+  0b0000000010100000, // 8
+  //1111111000000000
+  //6543210987654321  <- Led number
+};
+
+// Define the transitions between states
+// Each line represent a state and contains the possible next state transition
+// Must be 2 value. If only one choice is available, put it twice.
+byte transitions[][2] = {
+  {3, 6}, // state 0 Can go to state 3 or 6
+  {0, 0}, // state 1 End of game, back to state 0
+  {1, 1}, // state 2 Can go to state 1 only (twice the same)
+  {2, 4}, // state 3
+  {0, 0}, // state 4 
+  {0, 0}, // state 5
+  {5, 7}, // state 6
+  {8, 8}, // state 7
+  {0, 0}, // state 8
+};
+
+byte ledPins[] = {
+  2, 3, 4, 5, 6, 7, 8, 9
+};
+int nLeds = sizeof(ledPins);
